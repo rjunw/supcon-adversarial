1e28a5e101bdddb0eaed22409b3f457facfa349d
Ryan Wang <69535647+rjunw@users.noreply.github.com>
2023-05-03 20:06:27 -0400

    added plotting fn

--- explainability/explain_classifier.py
+++ explainability/explain_classifier.py
@@ -1,4 +1,6 @@
 from captum.attr import IntegratedGradients
+from utils.viz import visualize_image_attr
+from matplotlib.colors import LinearSegmentedColormap
 
 def explain_classifier(model, img, target, attr_kwargs = dict()):
     """
@@ -16,3 +18,18 @@ def explain_classifier(model, img, target, attr_kwargs = dict()):
                                target=target, 
                                **attr_kwargs)
     return attributes
+
+# Captum.ai IG plotting functions
+def visualize_classifier_exp(attr, img):
+    default_cmap = LinearSegmentedColormap.from_list('custom blue', 
+                                                 [(0, '#ffffff'),
+                                                  (0.25, '#000000'),
+                                                  (1, '#000000')], N=256)
+
+    _ = visualize_image_attr(attr.squeeze().cpu().detach().numpy(), # normalize over one channel/depth
+                                img.squeeze().cpu().detach().numpy(),
+                                method='heat_map',
+                                cmap=default_cmap,
+                                show_colorbar=True,
+                                sign='positive',
+                                outlier_perc=1)
